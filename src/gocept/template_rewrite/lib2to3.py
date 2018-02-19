7a0bec6fe828dd26ba04089bd8a1ef3d0835f3ca
Michael Howitz <mh@gocept.com>
2018-02-19 15:00:25 +0100

    Better error handling for parse errors.

--- src/gocept/template_rewrite/lib2to3.py
+++ src/gocept/template_rewrite/lib2to3.py
@@ -1,4 +1,10 @@
+import lib2to3.pgen2.parse
 import lib2to3.refactor
+import logging
+
+
+log = logging.getLogger(__name__)
+
 
 tool = lib2to3.refactor.RefactoringTool(
     lib2to3.refactor.get_fixers_from_package("lib2to3.fixes"))
@@ -7,7 +13,11 @@ tool = lib2to3.refactor.RefactoringTool(
 def rewite_using_2to3(src):
     """Rewrite a python expression using 2to3."""
     consolidated_src = src.lstrip()
-    tree = tool.refactor_string(consolidated_src + '\n', "<stdin>")
+    try:
+        tree = tool.refactor_string(consolidated_src + '\n', "<stdin>")
+    except lib2to3.pgen2.parse.ParseError:
+        log.error('*** SyntaxError: Cannot parse: %r', src, exc_info=True)
+        raise
     result = str(tree)[:-1]
     if result == consolidated_src:
         return src  # include leading white space
