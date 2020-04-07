2f6b9fe6d2a82bacb1cdcf5d6203a8d8a50e6850
Michael Howitz <mh@gocept.com>
2020-04-07 13:48:14 +0200

    Add more classifiers.

--- setup.py
+++ setup.py
@@ -36,13 +36,20 @@ setup(
 
     keywords='Zope DTML ZPT pagetemplates migrate Python 3',
     classifiers="""\
+Development Status :: 4 - Beta
+Framework :: Zope
+Intended Audience :: Developers
+License :: OSI Approved
 License :: OSI Approved :: MIT License
+Operating System :: OS Independent
+Programming Language :: Python
 Programming Language :: Python :: 3
 Programming Language :: Python :: 3 :: Only
 Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3.8
 Programming Language :: Python :: Implementation :: CPython
+Topic :: Text Processing :: Filters
 """[:-1].split('\n'),
     description=__doc__.strip(),
     long_description='\n\n'.join(open(name).read() for name in (
